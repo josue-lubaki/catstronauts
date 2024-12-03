52a6b4fa0738115ad2fb41a8e368722467991316
Josue Lubaki <josuelubaki@gmail.com>
2024-12-02 22:24:05 -0500

    resolvers & RESTDataSource added

---
+++ server/src/resolvers.ts
@@ -0,0 +1,19 @@
+
+export const resolvers = {
+    Query : {
+        // returns an array of Tracks that will be uses to populate
+        tracksForHome : (_, __, { dataSources }) => {
+            return dataSources.trackAPI.getTracksForHome();
+        }
+    },
+
+    // Instead, let's add another resolver specifically for a track's author. 
+    // To do this, we'll add another key to our resolvers object called Track, 
+    // indicating that it's for the Track type in our schema.
+    Track : {
+        author : ({ authorId }, _, { dataSources })=> {
+            return dataSources.trackAPI.getAuthor(authorId);
+        }
+    }
+}
+
